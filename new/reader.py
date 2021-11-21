be42ca4725de995f019f05ceab2d40c03361af10
derposoft <viswanath.chadalapaka@gmail.com>
2021-11-21 15:01:10 -0800

    +functional version!

--- new/reader.py
+++ new/reader.py
@@ -1,18 +1,15 @@
 import os
-import sys
-import csv
 import hashlib
-import numpy as np
 import pandas as pd
 from osu_map_parser.osu_map_parser.map_parser import MapParser
 from osrparse import parse_replay_file, GameMode, Mod
 
 # -2. create player name -> team name
 player_to_teamname = {}
-with open('teams.csv', 'r') as teams:
+with open('teams.csv', 'r', encoding='utf-8-sig') as teams:
     teams = teams.readlines()
     for team in teams:
-        team = team.split(',')
+        team = [x.strip() for x in team.split(',') if x.strip() != '']
         teamname = team[0]
         players = team[1:]
         for player in players:
@@ -31,41 +28,42 @@ mapmd5_to_id = {}
 for m in maps:
     map_md5 = hashlib.md5(open('maps/' + m,'rb').read()).hexdigest()
     map = MapParser('maps/' + m)
-    print(map.version)
-    if int(map.version) < 11:
+    if int(map.version) < 11: # TODO FIX!! IGNORING ALL OLD OSU MAP FILES!
         continue
     map_id = map.metadata['BeatmapSetID'] + '#osu/' + map.metadata['BeatmapID']
-    mapmd5_to_id[map_md5] = map_id
+    mapmd5_to_id[map_md5] = (map_id, map.metadata['Title'])
 
 # 1. extract zips
 zips = os.listdir('replays')
 for zip in zips:
     os.system('7z e "./replays/"' + zip +  ' -o"./replaysx/" -aoa')
 
-# 2. parse each map
+# 2. parse each replay and add score to results if valid replay
 replays = os.listdir('replaysx')
 maps_to_team = {} # maps map -> { team -> { player: score } }
 for r in replays:
     # 2.0. parse replay file
     replay = parse_replay_file('replaysx/' + r)
+    player = replay.player_name
     # 2.1. check game mode
     if replay.game_mode != GameMode.STD:
-        print('MAP NOT IN OSU!STD. SKIPPING...') # TODO
+        print(player + '\'s replay isn\t osu!std. SKIPPING...')
         continue
     # 2.2. check if replay is for map in pool
     replay_map_hash = replay.beatmap_hash
     if not replay_map_hash in mapmd5_to_id:
-        print('REPLAY NOT FOR A MAP IN POOL! SKIPPING...')
+        print(player + ' submitted a map not in the pool! SKIPPING...')
         continue
     # 2.3. check for scorev2 (for debug prints)
-    replay_map_id = mapmd5_to_id[replay_map_hash]
+    replay_map_id = mapmd5_to_id[replay_map_hash][0]
+    replay_map_name = mapmd5_to_id[replay_map_hash][1]
     allowed_mods = map_to_mods[replay_map_id]
     replay_mods = replay.mod_combination
     if not (replay_mods & Mod.ScoreV2):
         print('SCOREv2 NOT USED FOR THIS MAP. SKIPPING...')
         continue
     # 2.4. check the mods of the replay against allowed mods
-    MOD_ERROR_MSG = 'INCORRECT MODS FOR MAP! SKIPPING...'
+    MOD_ERROR_MSG = player + ' submitted ' + replay_map_name + ' with wrong map mods! SKIPPING...'
     def mod_check(mods, mod_flag):
         # we allow nf and require scorev2 for ALL mod flags. mod flag = 0 for NM
         if mods != (Mod.ScoreV2 | mod_flag) or mods != (Mod.ScoreV2 | Mod.NoFail | mod_flag):
@@ -90,13 +88,31 @@ for r in replays:
     if allowed_mods == 'FM':
         # anything is allowed and we've already checked for scorev2, so we don't have to do anything.
         pass
-    # 2.5. collect score and add to db
-    player = replay.player_name
+    # 2.5. collect score and add to result
     if player not in player_to_teamname:
-        print('PLAYER IS NOT IN A TEAM! WHY DID THIS GUY SUBMIT?? SKIPPING...')
+        print(player + ' isn\'t even a player... why did they submit?? SKIPPING...')
         continue
-    
-    sys.exit()
+    player_name = player
+    team_name = player_to_teamname[player]
+    score = replay.score
+    map_name = replay_map_name
+    if map_name not in maps_to_team:
+        maps_to_team[map_name] = {}
+    if team_name not in maps_to_team[map_name]:
+        maps_to_team[map_name][team_name] = {}
+    maps_to_team[map_name][team_name][player_name] = score
+print(maps_to_team)
+# 3. output all results as a csv
+output = pd.DataFrame()
+for map in maps_to_team:
+    # 3.1. for each team, get top scorer
+    for team in maps_to_team[map]:
+        top_player_score = 0
+        for player in maps_to_team[map][team]:
+            if maps_to_team[map][team][player] > top_player_score:
+                top_player_score = maps_to_team[map][team][player]
+        output = output.append([[map, team, player, top_player_score]])
+output.to_csv('outputs.csv', index=False, header=['map', 'team', 'player', 'score'])
 
 
 
