112baa5c7aab0399c9c39ac4b8366fbfee2097be
derposoft <viswanath.chadalapaka@gmail.com>
2021-11-21 15:09:52 -0800

    +setup file

---
+++ new/setup.py
@@ -0,0 +1,11 @@
+import os
+
+os.makedirs('map', exist_ok=True)
+os.makedirs('replays', exist_ok=True)
+os.makedirs('replaysx', exist_ok=True)
+
+with open('mapmods.csv', 'w') as mapmods:
+    mapmods.write('[BeatmapSetID]#osu/[BeatmapID],[NM/HD/HR/DT/FM]')
+with open('teams.csv', 'w') as teams:
+    teams.write('test team,Derposoft,Duhop')
+
